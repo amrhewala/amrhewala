062bc149afb2384d758ef541b5ecf766f45f0e56
amrhewala <53533564+amrhewala@users.noreply.github.com>
2024-12-26 21:46:10 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,6 +30,7 @@ export const Hero = () => {
           </a>
         </Button>
         <Button variant="outline" size="icon" className="rounded-full hover:scale-110 transition-transform hover:bg-accent/10 hover:border-accent">
+          <a href="a.hewala@gafi.gov.eg" target="_blank" rel="noopener noreferrer">
           <Mail className="h-5 w-5" />
         </Button>
         <Button variant="outline" size="icon" className="rounded-full hover:scale-110 transition-transform hover:bg-accent/10 hover:border-accent">
