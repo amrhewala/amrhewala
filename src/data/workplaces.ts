925ac1d41be45a26dfdf82ea00dfc08666ca86b3
amrhewala <53533564+amrhewala@users.noreply.github.com>
2025-01-05 11:53:18 +0200

    Update workplaces.ts

--- src/data/workplaces.ts
+++ src/data/workplaces.ts
@@ -3,18 +3,18 @@ export const workplaces = [
     name: "GAFI",
     logo: "/GAFI.svg",
     color: "#1a365d",
-    description: "Senior Data Specialist at the Egyptian Economic Authority, leading data analysis and visualization strategies."
+    description: "Senior Data Specialist at the Egyptian General Authority for Investment and free zones - GAFI, leading data analysis and visualization strategies."
   },
   {
     name: "Suez Canal University",
     logo: "/Suez_Canal_University.png",
     color: "#1a365d",
-    description: "Contributed to Business Administration and Management education."
+    description: "My mathematical dgree and Contributed to Business Administration and Management education."
   },
   {
     name: "Family Company s.r.o.",
     logo: "/lovable-uploads/Family_Company.png",
     color: "#1a365d",
-    description: "CEO for four years, successfully leading operations in Europe."
+    description: "CEO for four years, successfully leading operations in Europe - Slovakia ."
   }
 ];
